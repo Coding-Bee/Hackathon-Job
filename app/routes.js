032576874b41b8f7c87fa5e6dd42cc58c45298b8
Nischal Kanishk <nischalkanishk@gmail.com>
2020-02-28 11:44:06 +0530

    added  basic routes

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,13 @@
 module.exports = (app) => {
-    app.get('/', (req, res)=>res.send('Hello'))
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
+    
+    app.get('/user', (req, res)=>{
+        res.render('user');
+    })
+    app.get('/company', (req, res) =>{
+        res.render('company');
+    })
+    
 }
