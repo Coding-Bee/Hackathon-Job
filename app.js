e7d4ec825269a8fb2f87bb4fe4498bf1e1b7bf1d
Nischal Kanishk <nischalkanishk@gmail.com>
2020-02-28 11:36:45 +0530

    added route

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+require('./app/routes.js')(app);
+
 
 
 app.listen(3000, ()=> console.log(`Server started at ${port}`));
